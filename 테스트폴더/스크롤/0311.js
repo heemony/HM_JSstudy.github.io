86e3a1f73af3978952535c5177e17257b94a9e8d
heemyun <noodle@hivelab.co.kr>
2021-03-15 02:12:05 +0900

    Why Galaxy 코드 분석
    - 함수
    -- 즉시실행함수
    
    - 호이스팅, 스코프(체인)

--- 테스트폴더/스크롤/0311.js
+++ 테스트폴더/스크롤/0311.js
@@ -5,9 +5,9 @@
 })(this, function (global, isUndefined) { 
     "use strict";
 
-    var Component = (function () { // Component 함수 표현식
+    var Component = (function () { // 즉시실행함수를 Component변수에 할당.
         var win = global,
-            $ = win.jQuery,
+            $ = win.jQuery, // 즉시실행함수를 사용하는 이유 중 1. $를 제이쿼리를 위한 변수로 사용할 수 있다. (prototype에도 $변수가 있다고 한다..)
             doc = win.document,
             Util = win.WhyGalaxy.util,
             RESPONSIVE = win.WhyGalaxy.RESPONSIVE;
@@ -33,60 +33,71 @@
             };
             this.opts = Util.def(defParams, args || {}); // opts? Util안에 있는 def..?
             if (!(this.obj = $(this.opts.obj)).length) return;
-            this.init();
+            this.init(); 
         }
         Component.prototype = {
-            init: function () { 
-                this.initOpts();
-                this.buildPictures();
+            init: function () { // [처음에 실행되는 init함수]
+                this.initOpts(); // [1번째 호출]
+                this.buildPictures(); // [2번째 호출]
             }, 
+            // 1번째 함수 _ PC 인지 MO 인지 구별해주는 함수
             initOpts: function () {
-                this.winWidth = Util.winSize().w;
-                if (this.winWidth > RESPONSIVE.MOBILE.WIDTH) {
-                    if (this.opts.viewType !== 'PC') {
-                        this.opts.viewType = 'PC'
-                    }
+                this.winWidth = Util.winSize().w; // width 사이즈 재어와라.
+                if (this.winWidth > RESPONSIVE.MOBILE.WIDTH) { // 만약에 모바일보다 큰데, 
+                    if (this.opts.viewType !== 'PC') { // PC와는 다를 경우
+                        this.opts.viewType = 'PC' // PC 값으로 할당
+                    } 
                 } else if (this.winWidth <= RESPONSIVE.MOBILE.WIDTH) {
                     if (this.opts.viewType !== 'MO') {
-                        this.opts.viewType = 'MO'
+                        this.opts.viewType = 'MO' // MO 값으로 할당
                     }
                 }
             },
+            // 3번째 함수 _ 아래 함수들 다 실행해라
             loadAfter : function () {
-                this.buildScrollMagic();
-                this.buildCanvas();
-                this.buildParallax();
-                this.bindEvents(true);
+                this.buildScrollMagic(); // [4번째 호출]
+                this.buildCanvas(); // [5번째 호출]
+                this.buildParallax(); // [6번째 호출]
+                this.bindEvents(true); // [7번째 호출]
             },
+            // 2번째 함수 _ 빌드픽쳐스 (정의하는 함수인건가여?)
             buildPictures : function () {
-                Util.def(this, {
-                    pictures : {
+                Util.def(this, { // def? 정의하는 함수인가? 
+                    pictures : { // pictures라는 객체 프로퍼티
                         instance : null,
-                        destroy : $.proxy(function () {
-                            if (this.pictures.instance == null) return;
-                            this.pictures.instance.destroy();
-                            this.pictures.instance = null;
+                        // 없애는 메소드겠지 뭐..
+                        destroy : $.proxy(function () { 
+                            if (this.pictures.instance == null) return; // 픽쳐스 인스턴스 없으면 함수 빠져나가.
+                            this.pictures.instance.destroy(); // 있다면.destroy() 실행
+                            this.pictures.instance = null; // 그리고 null 값으로 할당.
                         }, this),
+                        // 로드하는 메소드겠지.. 뭐..
                         load : $.proxy(function (obj) {
-                            var deferred = $.Deferred();
-                            if (this.pictures.instance == null) {
+                            var deferred = $.Deferred(); 
+                            if (this.pictures.instance == null) { // 빈값이라면 아래 코드 실행.
+                                // obj와 on이라는 객체 프로퍼티를 갖는 PicturesLoaded라는 인스턴스 생성해라..? 
                                 this.pictures.instance = new PicturesLoaded(obj, { 
                                     on : {
                                         complete : $.proxy(function () {
-                                            deferred.resolve();
-                                        }, this)
+                                            deferred.resolve(); // resolve..해결됐다는건가..?
+                                        }, this) // 요 this는 뭐지???
                                     }
                                 });
                             }
-                            return deferred.promise();
-                        }, this)
+                            return deferred.promise(); // 프로미스..ㅠㅠ..?
+                        }, this) // 대체 이 this는 ㅜ머야..
                     }
                 });
+                // Component객체로 찍어난 인스턴스의 pictures가 로드됐다면 
                 this.pictures.load(this.obj).done($.proxy(function () {
-                    this.loadAfter();
+                    // loadAfter() 호출해라 이말인건가?
+                    this.loadAfter(); // [3번째 호출]
+                    // 그리고 obj에 클래스 붙여리.
                     this.obj.addClass(this.opts.classes.isLoaded);
                 }, this));
+
             },
+            // 5번째 함수 _ 빌드캔버스 (정의?)
             buildCanvas: function () {
                 Util.def(this, {
                     canvas : {
@@ -110,7 +121,7 @@
                         build : $.proxy(function(viewType) {
                             if (this.canvas.instance[viewType] !== null) return;
                             this.canvas.instance[viewType] = new CanvasBuild(this.obj, {
-                                viewType : viewType.toUpperCase()
+                                viewType : viewType.toUpperCase() // 대문자로 왜 만들어주나요? 6번쨰 함수 buildParallex도 마찬가지
                             });
                         }, this)
                     }
@@ -118,18 +129,19 @@
                 this.canvas.build(this.opts.viewType);
                 this.scrollmagic.scene.stickyScene.setTween(this.canvas.tween(this.opts.viewType));
             },
+            // 4번째 함수 _ scrollmagic 라이브러리 찾아보기
             buildScrollMagic : function () {
-                var triggerElement = this.obj.find(".wg-cinema-view__sticky").eq(0).get(0),
-                    stickyElement = triggerElement;
+                var triggerElement = this.obj.find(".wg-cinema-view__sticky").eq(0).get(0), // html에서 클래스 찾아서 가꾸와.
+                    stickyElement = triggerElement; // 변수명 바꿔서 재할당
                 var durationRatioSticky = 3;
                 var durationRatioAnimation = 1;
                 var durationSticky;
                 var durationAnimation;
                 if (Util.isDevice) {
-                    durationSticky = (Util.winSize().h * durationRatioSticky) + 'px';
-                    durationAnimation = (Util.winSize().h * durationRatioAnimation) + 'px';
+                    durationSticky = (Util.winSize().h * durationRatioSticky) + 'px';  // 3배 px
+                    durationAnimation = (Util.winSize().h * durationRatioAnimation) + 'px'; // 1배 px
                 } else {
-                    durationSticky = (100 * durationRatioSticky) + '%';
+                    durationSticky = (100 * durationRatioSticky) + '%'; 
                     durationAnimation = (100 * durationRatioAnimation) + '%';
                 }
                 Util.def(this, {
@@ -155,14 +167,17 @@
                                 },
                             }
                         },
+                        // 신 빌드
                         buildScene : $.proxy(function () {
                             if (this.scrollmagic.controller == null) return;
+                            // 고정되는 신
                             this.scrollmagic.scene.stickyScene = new ScrollMagic.Scene(this.scrollmagic.opts.stickyOpts.sceneOpts)
-                                .setPin(this.scrollmagic.opts.stickyOpts.stickyObj)
-                                .addTo(this.scrollmagic.controller);
+                                .setPin(this.scrollmagic.opts.stickyOpts.stickyObj) // set 프로퍼티..?
+                                .addTo(this.scrollmagic.controller); // 컨트롤러 붙여라~
+                            // 애니매이션 되는 신
                             this.scrollmagic.scene.animationScene = new ScrollMagic.Scene(this.scrollmagic.opts.animationOpts.sceneOpts)
                                 .addTo(this.scrollmagic.controller);
-                        }, this),
+                        }, this), // <- 대체 요 this가 머여
                         destroy : $.proxy(function () {
                             if (this.scrollmagic.controller == null) return;
                             this.scrollmagic.controller.destroy(true);
@@ -174,10 +189,11 @@
                         }, this)
                     }
                 });
-                this.scrollmagic.buildController();
-                this.scrollmagic.buildScene();
+                this.scrollmagic.buildController(); // 스크롤매직라이브러리의 buildController [4-1번째 호출]
+                this.scrollmagic.buildScene(); // [4-2번째 호출]
                 this.opts.setScroll = true;
             },
+            // 6번째 함수 _ (정의?)
             buildParallax : function () {
                 Util.def(this, {
                     parallax : {
@@ -197,7 +213,7 @@
                         build : $.proxy(function(viewType) {
                             if (this.parallax.instance[viewType] !== null) return;
                             this.parallax.instance[viewType] = new ParallaxBuild(this.obj, {
-                                viewType : viewType.toUpperCase()
+                                viewType : viewType.toUpperCase() 
                             });
                         }, this)
                     }
@@ -219,6 +235,7 @@
                 }
                 return events.join(" ");
             },
+            // 7번째 함수 _ 
             bindEvents: function (type) {
                 if (type) {
                     $(win).on(this.changeEvents('resize orientationchange'), $.proxy(this.resizeFunc, this));
@@ -274,6 +291,7 @@
                 }
                 this.opts.resizeAttr.w = winWidth;
             },
+            // out..call..back..?...뭐징..
             outCallback: function (ing) {
                 var callbackObj = this.opts[ing];
                 if (callbackObj == null) return;
@@ -322,8 +340,8 @@
                     canvasAnimation : {
                         instance : null,
                         basicProp : {},
-                        prop : {
-                            PC : {
+                        prop : { // PC일 때 scale 4 -> 1, MO 일 때 scale 5 -> 1 로 축소되는 prop 객체 프로퍼티
+                            PC : { 
                                 from : {
                                     scale : 4
                                 },
@@ -331,7 +349,7 @@
                                     scale : 1
                                 }
                             },
-                            MO : {
+                            MO : { 
                                 from : {
                                     scale : 5
                                 },
@@ -403,7 +421,7 @@
                 Util.def(this, { // [?] Util.def는 정의하는 함수인가?
                     bgAnimation : { // bgAnimation이라는 객체
                         instance : null, 
-                        prop : { // PC는 1.2 -> 1, MO는 1.4 -> 1 로 축소되는 prop 프로퍼티
+                        prop : { // PC는 1.2 -> 1, MO는 1.4 -> 1 로 축소되는 prop 객체 프로퍼티
                             PC : {
                                 backgoundScale : {
                                     from : {
@@ -428,7 +446,7 @@
                         scale : $.proxy(function (prop) { // [?] 이 코드에서 마지막 this로 뭘 하는거지?
                             TweenLite.to(this.bgWrap, .7, prop) 
                             // TweenLite 뭐죠, 
-                            // 캔버스(component)의 변수 bgWrap는 '.iamge' , 0.7초 동안 실행하라?
+                            // 캔버스(component)의 변수 bgWrap는 '.iamge'이고, 0.7초 동안 실행하라? 뭐 그런의미일까
                         }, this)
                     }
                 });
@@ -494,13 +512,13 @@
             }
             this.opts = Util.def(defParams, args || {});
             if (!(this.obj = $(this.opts.obj)).length) return;
-            this.init();
+            this.init(); // [init 호출]
         }
         ParallaxBuild.prototype = {
-            init: function () {
-                this.setElements();
-                this.buildBgAnimation();
-                this.buildTween();
+            init: function () { // init 메소드
+                this.setElements();  // [1번째 함수 호출]
+                this.buildBgAnimation();  // [2번째 함수 호출]
+                this.buildTween();  // [3번째 함수 호출]
             },
             setElements : function () {
                 this.bgWrap = this.obj.find(this.opts.backgroundWrap);
@@ -509,7 +527,7 @@
                 Util.def(this, {
                     bgAnimation : {
                         instance : null,
-                        prop : {
+                        prop : { // PC : 300 -> 0, MO : 280 -> 0 
                             PC : {
                                 from : {
                                     y : 300
@@ -527,8 +545,8 @@
                                 }
                             }
                         },
-                        move : $.proxy(function (prop) {
-                            TweenLite.to(this.bgWrap, .7, prop)
+                        move : $.proxy(function (prop) { // prop이 움직이는데 걸리는 시간 0.7s
+                            TweenLite.to(this.bgWrap, .7, prop) // TweenLite는 뭘 의미?
                         }, this)
                     }
                 })
@@ -573,14 +591,6 @@
     })();
     return Component; // 컴포넌트 돌려줘라.
 });
-/* 
-    function 함수(){}; 
-    함수.prototype = {} 
-    형태의 코드 패턴(?)
-    
-    함수 안에서 함수를 만들면 new로 인스턴스를 찍어낼 때 마다 (프로퍼티, 메서드들 같은거..) 각각 생기기 때문에 메모리를 마니 먹는당..
-    같은 유형간에 공유하는 객체를 프로토타입이라 하는데, 프로토타입 체인이 그 공유하는 객체를 바라보게 해주면 공유를 할 수 있다~
-*/
 
 (function (global, factory) {
     $(function () {
@@ -589,7 +599,7 @@
 })(this, function (global, isUndefined) {
     "use strict";
 
-    var Component = (function () { // 익명 함수 표현식
+    var Component = (function () { 
         var win = global,
             $ = win.jQuery,
             Util = win.WhyGalaxy.util;
@@ -619,21 +629,18 @@
 });
 
 
-
 /* 
-    // 1. Proxy : 새로운 행동을 정의할 때 사용.
-    Proxy 객체는 기본적인 동작(속성 접근, 할당, 순회, 열거, 함수 호출) 등
-
-    new Proxy(target, handler)
-    - target : native array, function, 다른 proxy를 포함한 객체.
-    - handler : 프로퍼티들이 function인 객체. 동작이 수행될 때 handler는 Proxy의 행동을 정의.
-
-
-*/
+ *[ 프로토타입 - 아래 형태의 코드 패턴을 사용하는 이유 => 메모리 절감]
+ * function 함수(){}; 
+ * 함수.prototype = {} 
+ * 
+ * 함수 안에서 함수를 만들면 new로 인스턴스를 찍어낼 때 마다 (프로퍼티(k:v), 메서드(f)들 같은거..) 각각 생기기 때문에 메모리를 마니 먹는당..
+ * 같은 유형간에 공유하는 객체를 프로토타입이라 하는데, 프로토타입 체인이 그 공유하는 객체를 바라보게 해주면 공유를 할 수 있다~
+ */
 
 /*
-    // 2. 자체 호출 익명 함수
-    
+ * [ 2. 자체 호출 익명 함수 ]
+ 
     function add(a, b) {
         return a + b;
     }
@@ -648,9 +655,22 @@
 
     (function(a, b) {
         return a + b;
-    })(10, 2);와 같다.
+    })(10, 2); 
+    와 같다.
 
-    자체 호출 익명 함수, 실행되고 버려짐. 해당 범위에만 개인 변수를 포함한다. 클로저 내에도 저장되지 않는다.
+    즉시실행함수 
+    : 실행되고 버려짐. 해당 범위에만 개인 변수를 포함한다. 클로저 내에도 저장되지 않는다. 프로토타입의 변수 $의 오버라이팅 방지(제이쿼리 변수로 사용할 수 있다.)
+    : 하나의 파일안에 모든 코드를 담는 것은 불가능하기에, js 함수의 특징을 이용하여 모듈화를 할 수 있다. 
+    
 
+*/
 
+/* 
+    // [ Proxy : 새로운 행동을 정의할 때 사용 ]
+
+    Proxy 객체는 기본적인 동작(속성 접근, 할당, 순회, 열거, 함수 호출) 등
+
+    new Proxy(target, handler)
+    - target : native array, function, 다른 proxy를 포함한 객체.
+    - handler : 프로퍼티들이 function인 객체. 동작이 수행될 때 handler는 Proxy의 행동을 정의.
 */
